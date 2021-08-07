694af2e41c8703a161a61ef0caa4ae6a6911daa8
jinyedo <whereyedo@naver.com>
2021-08-07 15:49:35 +0900

    상대방과 자신을 구분하기

--- src/main/java/com/example/chatting/handler/WebSocketHandler.java
+++ src/main/java/com/example/chatting/handler/WebSocketHandler.java
@@ -1,5 +1,9 @@
 package com.example.chatting.handler;
 
+import lombok.extern.log4j.Log4j2;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -9,6 +13,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.HashMap;
 
 @Component
+@Log4j2
 public class WebSocketHandler extends TextWebSocketHandler {
 
     HashMap<String, WebSocketSession> sessionHashMap = new HashMap<>(); // 웹소켓 세션을 담아둘 맵
@@ -17,15 +22,22 @@ public class WebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
         sessionHashMap.put(session.getId(), session);
+        JSONObject obj = new JSONObject();
+        obj.put("type", "getId");
+        obj.put("sessionId", session.getId());
+        session.sendMessage(new TextMessage(obj.toJSONString()));
     }
 
     @Override // 메시지 발송
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info("----------------------------메시지 발송---------------------------");
         String msg = message.getPayload();
+        log.info(msg);
+        JSONObject obj = jsonToObjectParse(msg);
         for (String key : sessionHashMap.keySet()) {
             WebSocketSession wss = sessionHashMap.get(key);
             try {
-                wss.sendMessage(new TextMessage(msg));
+                wss.sendMessage(new TextMessage(obj.toJSONString()));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -36,5 +48,16 @@ public class WebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         sessionHashMap.remove(session.getId());
         super.afterConnectionClosed(session, status);
+    }
+
+    private static JSONObject jsonToObjectParse(String jsonStr) {
+        JSONParser parser = new JSONParser();
+        JSONObject obj = null;
+        try {
+            obj = (JSONObject) parser.parse(jsonStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return obj;
     }
 }

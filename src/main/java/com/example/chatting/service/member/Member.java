49b1b03a689a732496a1b90b4fd3d727459c9bc3
jinyedo <whereyedo@naver.com>
2021-08-11 13:56:08 +0900

    회원 entity 생성

--- src/main/java/com/example/chatting/service/member/Member.java
+++ src/main/java/com/example/chatting/service/member/Member.java
@@ -1,6 +1,7 @@
 package com.example.chatting.service.member;
 
 import com.example.chatting.base.BaseEntity;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,8 +9,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+@ToString
 public class Member extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long mid;
+    private Long memberId;
+
+    private String username; // 아이디
+
+    private String password;
+
+    private String name;
 }
